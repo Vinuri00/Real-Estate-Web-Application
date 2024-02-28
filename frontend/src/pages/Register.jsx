77c230b805c22e6b8aa2692f9a485f5fbc1502d6
Vinuri Disara <vinuridisara2020@gmail.com>
2024-02-28 13:16:58 +0530

    update the register form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -25,8 +25,81 @@ const Register = () => {
     areaOfExpertise: "",
   });
 
+  // ----------------------------------------- JavaScript Vaalidation ---------------------------------------------
+
+  const [errors, setErrors] = useState({});
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    let errorMessage = "";
+
+    switch (name) {
+      case "fullName":
+        if (value.trim() === "") {
+          errorMessage = "Full Name is required!";
+        }
+        break;
+
+      case "email":
+        if (!/\S+@\S+.\S+/.test(value)) {
+          errorMessage = "Email is not valid!";
+        }
+        break;
+
+      case "password":
+        if (
+          value.length < 6 ||
+          !/[A-Z]/.test(value) ||
+          !/[!@#$%^&*]/.test(value)
+        ) {
+          errorMessage =
+            "Password must be at least 6 characters! include: \n Should include at least one capital letter! \n Should include at least one symbol!";
+        }
+        break;
+
+      case "phone":
+        if (!/^\+\d{1,3} \d{3} \d{3} \d{3}$/.test(value)) {
+          errorMessage = "Phone number is not valid";
+        }
+        break;
+
+      case "address":
+        if (value.trim() === "") {
+          errorMessage = "Address is required";
+        }
+        break;
+
+      case "companyName":
+        if (value.trim() === "") {
+          errorMessage = "Company Name is required";
+        }
+        break;
+
+      case "licenseNumber":
+        if (value.trim() === "") {
+          errorMessage = "License Number is required";
+        }
+        break;
+
+      case "yearsOfExperience":
+        if (value.trim() === "") {
+          errorMessage = "Years of Experience is required";
+        }
+        break;
+
+      case "areaOfExpertise":
+        if (value.trim() === "") {
+          errorMessage = "Area of Expertise is required";
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    setErrors({ ...errors, [name]: errorMessage });
+    setFormData({ ...formData, [name]: value });
+    // setFormData({ ...formData, [e.target.name]: e.target.value });
+    // setFormData({ ...formData, [name]: value, ["${name}Error"]: errorMessage });
   };
 
   const handleSubmit = async (e) => {
@@ -39,15 +112,33 @@ const Register = () => {
     }
   };
 
+  // ----------------------------------------- JavaScript Vaalidation ---------------------------------------------
+
+  // const handleChange = (e) => {
+  //   const { name, value } = ee.target;
+  //   let errorMessage = "";
+
+  //   switch (name) {
+  //     case "fullName":
+  //       if (value.trim() === "") {
+  //         errorMessage = "Full Name is required";
+  //       }
+
+  //       setFormData({ ...formData, [name]: value, ['${name}Error']: errorMessage});
+  //   };
+  // }
+
+  // --------------------------------------------------------------------------------------------------------------
+
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen mt-16">
       <div className="bg-white p-8 rounded shadow-2xl w-1/2">
         <h2 className="text-2xl font-bold text-center">
           {/* top spacing -> mb-9 */}
           Create an Account
         </h2>
 
-        <form onSubmit={handleSubmit} className="space-y-0">
+        <form onSubmit={handleSubmit} className="space-y-0 mt-5">
           <div className="grid grid-cols-2 gap-4">
             {/* Name */}
             <div className="mb-5">
@@ -93,8 +184,8 @@ const Register = () => {
               </label>
 
               <input
-                type="password"
-                id="password-success"
+                type="email"
+                id="emai-success"
                 className="bg-green-50 border border-green-500 text-black 
               dark:text-black 
               placeholder-green-700 
@@ -247,7 +338,7 @@ const Register = () => {
               <div className="mb-5">
                 <label
                   htmlFor="licenseNumber"
-                  className="block mb-4 font-medium text-green-800"
+                  className="block mt-4 font-medium text-green-800"
                 >
                   License Number
                 </label>
@@ -263,7 +354,7 @@ const Register = () => {
                   focus:border-green-700 
                   dark:bg-green-100 
                   dark:border-green-500
-                  text-sm rounded-lg block w-full p-2.5"
+                  text-sm rounded-lg block w-full p-2.5 mt-4"
                   placeholder="License Number"
                 />
               </div>
@@ -272,7 +363,7 @@ const Register = () => {
               <div className="mb-5">
                 <label
                   htmlFor="yearsOfExperience"
-                  className="block mb-4 font-medium text-green-800"
+                  className="block mt-4 font-medium text-green-800"
                 >
                   Years of Experience
                 </label>
@@ -288,7 +379,7 @@ const Register = () => {
                   focus:border-green-700 
                   dark:bg-green-100 
                   dark:border-green-500
-                  text-sm rounded-lg block w-full p-2.5"
+                  text-sm rounded-lg block w-full p-2.5 mt-4"
                   placeholder="Years of Experience"
                 />
               </div>
@@ -329,12 +420,21 @@ const Register = () => {
           <div className="flex justify-center">
             <button
               type="submit"
-              className="w-full md:w-2/3 bg-green-600 hover:bg-green-900 text-white px-4 py-1.5 rounded"
+              className="w-full md:w-9/10 bg-green-600 hover:bg-green-900 text-white px-4 py-1.5 rounded mt-8"
             >
               Create Account
             </button>
           </div>
         </form>
+
+        {/* Login Link */}
+        <div className="mt-4 text-center">
+          Already have an account?
+          &nbsp;
+          <a href="/login" className="text-green-600 hover:underline">
+            Login here
+          </a>
+        </div>
       </div>
     </div>
   );
