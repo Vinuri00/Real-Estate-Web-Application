e3f0187511f62b5e288b915f6ea59e60d2193672
Vinuri Disara <vinuridisara2020@gmail.com>
2024-03-21 23:16:07 +0530

    Added docker deployement function in admin

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -6,11 +6,16 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+
   server: {
     host: true,
-    port: 5173, // This is the port which we will use in docker
-    // Thanks @sergiomoura for the window fix
-    // add the next lines if you're using windows and hot reload doesn't work
+    port: 5173,
+    strictPort: true,
     watch: {
       usePolling: true
     }
