eb2f783e9e4c5a96b24f38c545b553f647d2c1f0
Vinuri Disara <vinuridisara2020@gmail.com>
2024-03-21 21:27:01 +0530

    Update CORS origin in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const corsData = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:5000"],
   credentials: true,
   optionsuccessStatus: 200,
 };
