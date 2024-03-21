a0ecc4172b6f641216f6eb733082892668b16661
Vinuri Disara <vinuridisara2020@gmail.com>
2024-03-21 21:26:46 +0530

    Update LawyerModel properties

--- Admin/src/models/lawyers.js
+++ Admin/src/models/lawyers.js
@@ -1,11 +1,12 @@
 export const LawyerModel = {
     _id: null,
     fullName: null,
-    username: null,
+    userName: null,
     email: null,
     contactNumber: null,
     password: null,
     companyName: null,
     licenseNumber: null,
     yearsOfExperience: null,
+    status: null,
 };
