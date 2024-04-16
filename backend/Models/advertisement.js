acc12ec10054389a9fc9af1e5edcf7ae331cd80a
Vinuri Disara <vinuridisara2020@gmail.com>
2024-04-16 21:02:44 +0530

    Add Advertisement model to backend

---
+++ backend/Models/advertisement.js
@@ -0,0 +1,39 @@
+import pkg from "mongoose";
+const { Schema, model, models } = pkg;
+// import mongooseUniqueValidator from "mongoose-unique-validator";
+
+const AdvertisementSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, "Please enter the title"],
+    },
+
+    description: {
+        type: String,
+        required: [true, "Please enter the description"],
+    },
+
+    image: {
+        type: String,
+        required: [true, "Please add images of the land"],
+    },
+
+    price: {
+        type: Number,
+        required: [true, "Please enter the price"],
+    },
+
+    Location: {
+        type: String,
+        required: [true, "Please enter the location"],
+    },
+
+    status: {
+        type: Number,
+        default: 0,         // 0 - Pending, 1 - Approved, 2 - Rejected
+    }
+});  
+
+const Advertisement = models.Advertisement || model("Advertisement", AdvertisementSchema);
+
+export default Advertisement;
