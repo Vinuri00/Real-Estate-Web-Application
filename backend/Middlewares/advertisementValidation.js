8b70dd9a3e1f4898ed80ace6781d8a84abef45cb
Vinuri Disara <vinuridisara2020@gmail.com>
2024-04-16 21:22:28 +0530

    Add advertisement validation middleware

---
+++ backend/Middlewares/advertisementValidation.js
@@ -0,0 +1,34 @@
+export const validateAdvertisement = (req, res, next) => {
+    const { title, description, ownerName, contactNumber, image, price, Location } = req.body;
+
+    if (!title) {
+        return res.status(400).json({ message: "Please enter the title" });
+    }
+
+    if (!description) {
+        return res.status(400).json({ message: "Please enter the description" });
+    }
+
+    if (!ownerName) {
+        return res.status(400).json({ message: "Please enter the owner's name" });
+    }
+
+    if (!contactNumber) {
+        return res.status(400).json({ message: "Please enter contact number" });
+    }
+
+    if (!image) {
+        return res.status(400).json({ message: "Please add images of the land" });
+    }
+
+    if (!price) {
+        return res.status(400).json({ message: "Please enter the price" });
+    }
+
+    if (!location) {
+        return res.status(400).json({ message: "Please enter the location" });
+    }
+
+    next();
+};
+
