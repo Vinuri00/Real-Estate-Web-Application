470be0abfb8ea29fb80416a0d76656443cdf6728
Vinuri Disara <vinuridisara2020@gmail.com>
2024-04-21 00:12:40 +0530

    Update API calls and add new routes for lawyer registration and user registration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const CREATE_ADVERTISEMENTS = "/advertisements/create";
-// export const REGISTER_URL = "/lawyers/register";
+export const LAWYERS_REGISTER = "/lawyers/register";
+export const LOGIN_URL = "/lawyers/login";
+export const USER_REGISTER = "/users/register";
 
 export default axios.create({
   baseURL: import.meta.env.VITE_BASEURL,
