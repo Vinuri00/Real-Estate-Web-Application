942e5e8e5d489ea2bcc6e9cec2f8a9afe76ae56a
Vinuri Disara <vinuridisara2020@gmail.com>
2024-04-22 01:24:09 +0530

    Refactor userRouter in users.js to improve code readability and add user registration and login functionality

--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -3,9 +3,9 @@ import connection from "../Database/conn.js";
 import express from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { 
-    validateUserLogin, 
-    validateUserRegister
+import {
+  validateUserLogin,
+  validateUserRegister,
 } from "../Middlewares/userValidation.js";
 
 dotenv.config();
@@ -14,93 +14,80 @@ const userRouter = express.Router();
 
 // Register new user
 userRouter.post("/register", validateUserRegister, async (req, res) => {
-    const {
-        fullName,
-        userName,
-        email,
-        contactNumber,
-        password,
-    } = req.body;
+  const { fullName, userName, email, contactNumber, password } = req.body;
 
-    console.log(
-        userName,
-        email,
-        contactNumber,
-        password,
+  console.log(userName, email, contactNumber, password);
+
+  await connection();
+
+  // adding an user
+  try {
+    const user = new User({
+      fullName,
+      userName,
+      email,
+      contactNumber,
+      password,
+    });
+
+    user.password = user.encryptUserPassword(password);
+
+    await user.save();
+
+    console.log(user);
+
+    if (!user) {
+      return res.status(400).json({ message: "User not added" });
+    }
+
+    // Generate token for the user when they register
+    const token = jwt.sign(
+      { userId: user._id, userName: user.fullName },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 3600,
+      }
     );
 
-    await connection();
-
-    // adding an user
-    try {
-        const user = new User({
-            fullName,
-            userName,
-            email,
-            contactNumber,
-            password,
-        });
-
-        user.password = user.encryptUserPassword(password);
-
-        await user.save();
-
-        console.log(user);
-
-        if (!user) {
-            return res.status(400).json({ message: "User not added" });
-        }
-
-        // Generate token for the user when they register
-        const token = jwt.sign(
-            { userId: user._id, userName: user.fullName },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: 3600,
-            }
-        );
-
-        res.status(201).json({token});
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: error.message });
-    }
+    res.status(201).json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 // User login
 userRouter.post("/login", validateUserLogin, async (req, res) => {
-    const { userName, password } = req.body;
+  const { userName, password } = req.body;
 
-    await connection();
+  await connection();
 
-    try {
-        const user = await User.findOne({ userName });
+  try {
+    const user = await User.findOne({ userName });
 
-        if (!user) {
-            return res.status(400).json({ message: "User not found" });
-        }
-
-        const isMatch = user.validatePassword(password, user.password);
-
-        if (!isMatch) {
-            return res.status(400).json({ message: "Invalid credentials" });
-        }
-
-        // Generate token for the user when they login
-        const token = jwt.sign(
-            { userId: user._id, userName: user.fullName },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: 3600,
-            }
-        );
-
-        res.status(200).json({ token });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
+
+    const isMatch = user.validatePassword(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Generate token for the user when they login
+    const token = jwt.sign(
+      { userId: user._id, userName: user.fullName },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 3600,
+      }
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
-
-
 
 export default userRouter;
