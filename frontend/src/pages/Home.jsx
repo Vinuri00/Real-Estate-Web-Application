79cd25125a5c884561c7f929be7cbbf23474c4c2
Vinuri Disara <vinuridisara2020@gmail.com>
2024-04-24 01:40:05 +0530

    Update Home page background image and content

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import blocklandbg from "../assets/images/blocklandbg.webp";
+// import blocklandbg from "../assets/images/blocklandbg.webp";
 
 const Home = () => {
   const [showLawyers, setShowLawyers] = useState(false);
@@ -7,7 +7,7 @@ const Home = () => {
 
   return (
     <div>
-      <div className="relative bg-gray-800 h-screen">
+      {/* <div className="relative bg-gray-800 h-screen">
         <img
           className="w-full h-full object-cover brightness-50"
           src={blocklandbg}
@@ -20,6 +20,8 @@ const Home = () => {
             <h1 className="text-4xl font-bold text-white mb-4">
               Welcome to Our Website
             </h1>
+
+            // select one of the two options
 
             <div className="flex justify-center mb-4">
               <div className="border-green-600 border rounded-lg p-4 mr-4 text-white font-bold flex items-center">
@@ -48,7 +50,70 @@ const Home = () => {
             Learn More
           </button>
         </div>
-      </div>
+      </div> */}
+
+      <section className="bg-center bg-no-repeat bg-[url('assets/images/blocklandbg.webp')] bg-gray-700 bg-blend-multiply">
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+            Find Your Perfect Plot
+          </h1>
+          <p className="mb-5 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-40">
+            Explore prime real estate opportunities where technology,
+            innovation, and capital converge for long-term value and economic
+            growth. Benefit from our legal services and connect with trusted
+            brokers.
+          </p>
+          <p className="mb-8 text-lg font-bold text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+            <i>Start investing today.</i>
+          </p>
+
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <button
+              className={`bg-green-600 rounded-lg p-4 text-white font-bold flex items-center ${
+                showLawyers ? "bg-green-700" : ""
+              }`}
+              onClick={() => setShowLawyers(!showLawyers)}
+            >
+              View Lawyers
+            </button>
+            <button
+              className={`bg-green-600 rounded-lg p-4 text-white font-bold flex items-center ml-4 ${
+                showBrokers ? "bg-green-700" : ""
+              }`}
+              onClick={() => setShowBrokers(!showBrokers)}
+            >
+              Brokerage Details
+            </button>
+            {/* <a
+              href="#"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900"
+            >
+              Get started
+              <svg
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+            <a
+              href="#"
+              className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
+            >
+              Learn more
+            </a> */}
+          </div>
+        </div>
+      </section>
 
       <div className="container mx-auto px-10 pt-10">
         {/* <h2 className="text-3xl font-semibold text-gray-800 mb-8">Sections</h2> */}
@@ -305,7 +370,6 @@ const Home = () => {
             <p className="text-3xl font-bold pt-3">Acheivement Awards</p>
           </div>
         </div>
-
       </div>
     </div>
   );
