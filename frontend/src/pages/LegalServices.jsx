2f7fb545c9509170867788cd5cf4d3f80e9d5802
Vinuri Disara <vinuridisara2020@gmail.com>
2024-04-24 18:32:48 +0530

    Update tax-related stress message in LegalServices.jsx

--- frontend/src/pages/LegalServices.jsx
+++ frontend/src/pages/LegalServices.jsx
@@ -3,21 +3,24 @@ import React from "react";
 const LegalServices = () => {
   return (
     <>
-      <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+      <section className="bg-center bg-cover bg-no-repeat bg-[url('assets/images/legal1.webp')] bg-gray-600 bg-blend-multiply">
         <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
             Trust Us to Safeguard Your Real Estate Investments
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-24">
-            Don't stress over taxes. Hand them off to a pro for <strong className="font-extrabold"><i>HALF OFF </i></strong>  
-            and relax.
-            Say goodbye to tax-related stress and let us handle your legal needs.
-            Contact us today to schedule an appointment and take advantage of this exclusive offer!
+            Don't stress over taxes. Hand them off to a pro for{" "}
+            <strong className="font-extrabold">
+              <i>HALF OFF </i>
+            </strong>
+            and relax. Say goodbye to tax-related stress and let us handle your
+            legal needs. Contact us today to schedule an appointment and take
+            advantage of this exclusive offer!
           </p>
           <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <a
               href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900"
             >
               Get started
               <svg
@@ -42,6 +45,213 @@ const LegalServices = () => {
             >
               Learn more
             </a>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-white dark:bg-green-950">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
+          {/* <div className="bg-gray-50 dark:bg-white border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+            <a
+              href="#"
+              className="bg-blue-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2"
+            >
+              <svg
+                className="w-2.5 h-2.5 me-1.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 14"
+              >
+                <path d="M11 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm8.585 1.189a.994.994 0 0 0-.9-.138l-2.965.983a1 1 0 0 0-.685.949v8a1 1 0 0 0 .675.946l2.965 1.02a1.013 1.013 0 0 0 1.032-.242A1 1 0 0 0 20 12V2a1 1 0 0 0-.415-.811Z" />
+              </svg>
+              Tutorial
+            </a>
+            <h1 className="text-gray-900 dark:text-black text-3xl md:text-5xl font-extrabold mb-2">
+              How to quickly deploy a static website
+            </h1>
+            <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <a
+              href="#"
+              className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900"
+            >
+              Read more
+              <svg
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div> */}
+          <div className="grid md:grid-cols-3 gap-8">
+            <div className="bg-gray-50 dark:bg-white border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+              <a
+                href="#"
+                className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-green-950 dark:text-green-400 mb-2"
+              >
+                {/* <svg
+                  className="w-2.5 h-2.5 me-1.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 18"
+                >
+                  <path d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
+                </svg> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-4 h-4 me-1.5"
+                >
+                  <path d="M2 4.5A2.5 2.5 0 0 1 4.5 2h11a2.5 2.5 0 0 1 0 5h-11A2.5 2.5 0 0 1 2 4.5ZM2.75 9.083a.75.75 0 0 0 0 1.5h14.5a.75.75 0 0 0 0-1.5H2.75ZM2.75 12.663a.75.75 0 0 0 0 1.5h14.5a.75.75 0 0 0 0-1.5H2.75ZM2.75 16.25a.75.75 0 0 0 0 1.5h14.5a.75.75 0 1 0 0-1.5H2.75Z" />
+                </svg>
+                Design
+              </a>
+              <h2 className="text-gray-900 dark:text-black text-3xl font-extrabold mb-2">
+                Start with Flowbite Design System
+              </h2>
+              <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+                Static websites are now used to bootstrap lots of websites and
+                are becoming the basis for a variety of tools that even
+                influence both web designers and developers.
+              </p>
+              <a
+                href="#"
+                className="text-green-600 dark:text-green-500 hover:underline font-medium text-lg inline-flex items-center"
+              >
+                Read more
+                <svg
+                  className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="bg-gray-50 dark:bg-white border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+              <a
+                href="#"
+                className="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-green-950 dark:text-yellow-400 mb-2"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-4 h-4 me-1.5"
+                >
+                  <path d="M19.006 3.705a.75.75 0 1 0-.512-1.41L6 6.838V3a.75.75 0 0 0-.75-.75h-1.5A.75.75 0 0 0 3 3v4.93l-1.006.365a.75.75 0 0 0 .512 1.41l16.5-6Z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M3.019 11.114 18 5.667v3.421l4.006 1.457a.75.75 0 1 1-.512 1.41l-.494-.18v8.475h.75a.75.75 0 0 1 0 1.5H2.25a.75.75 0 0 1 0-1.5H3v-9.129l.019-.007ZM18 20.25v-9.566l1.5.546v9.02H18Zm-9-6a.75.75 0 0 0-.75.75v4.5c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75V15a.75.75 0 0 0-.75-.75H9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Code
+              </a>
+              <h2 className="text-gray-900 dark:text-black text-3xl font-extrabold mb-2">
+                Best react libraries around the web
+              </h2>
+              <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+                Static websites are now used to bootstrap lots of websites and
+                are becoming the basis for a variety of tools that even
+                influence both web designers and developers.
+              </p>
+              <a
+                href="#"
+                className="text-green-600 dark:text-green-500 hover:underline font-medium text-lg inline-flex items-center"
+              >
+                Read more
+                <svg
+                  className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="bg-gray-50 dark:bg-white border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+              <a
+                href="#"
+                className="bg-purple-100 text-yellow-300 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-green-950 dark:text-blue-400 mb-2"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-4 h-4 me-1.5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.5 5.25a3 3 0 0 1 3-3h3a3 3 0 0 1 3 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0 1 12 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 0 1 7.5 5.455V5.25Zm7.5 0v.09a49.488 49.488 0 0 0-6 0v-.09a1.5 1.5 0 0 1 1.5-1.5h3a1.5 1.5 0 0 1 1.5 1.5Zm-3 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+                    clipRule="evenodd"
+                  />
+                  <path d="M3 18.4v-2.796a4.3 4.3 0 0 0 .713.31A26.226 26.226 0 0 0 12 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 0 1-6.477-.427C4.047 21.128 3 19.852 3 18.4Z" />
+                </svg>
+                Code
+              </a>
+              <h2 className="text-gray-900 dark:text-black text-3xl font-extrabold mb-2">
+                Best react libraries around the web
+              </h2>
+              <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+                Static websites are now used to bootstrap lots of websites and
+                are becoming the basis for a variety of tools that even
+                influence both web designers and developers.
+              </p>
+              <a
+                href="#"
+                className="text-green-600 dark:text-green-500 hover:underline font-medium text-lg inline-flex items-center"
+              >
+                Read more
+                <svg
+                  className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </section>
