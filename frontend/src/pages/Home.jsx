a91e487c465bf681f1be59aead0fe9d940cb1ac7
Vinuri Disara <vinuridisara2020@gmail.com>
2024-05-05 11:28:28 +0530

    Update Home page background image and remove commented out code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,51 +7,6 @@ const Home = () => {
 
   return (
     <div>
-      {/* <div className="relative bg-gray-800 h-screen">
-        <img
-          className="w-full h-full object-cover brightness-50"
-          src={blocklandbg}
-          alt="Background"
-          style={{ filter: "brightness(30%" }}
-        />
-
-        <div className="absolute inset-0 flex justify-center items-center flex-col">
-          <div className="text-center mb-8">
-            <h1 className="text-4xl font-bold text-white mb-4">
-              Welcome to Our Website
-            </h1>
-
-            // select one of the two options
-
-            <div className="flex justify-center mb-4">
-              <div className="border-green-600 border rounded-lg p-4 mr-4 text-white font-bold flex items-center">
-                <input
-                  type="checkbox"
-                  className="mr-2 bg-green-600 w-5 h-5"
-                  checked={showLawyers}
-                  onChange={() => setShowLawyers(!showLawyers)}
-                />
-                <label className="select-none">Lawyer Services</label>
-              </div>
-
-              <div className="border-green-600 border rounded-lg p-4 text-white font-bold flex items-center">
-                <input
-                  type="checkbox"
-                  className="mr-2 bg-green-600 w-5 h-5"
-                  checked={showBrokers}
-                  onChange={() => setShowBrokers(!showBrokers)}
-                />
-                <label className="select-none">Brokerage Services</label>
-              </div>
-            </div>
-          </div>
-
-          <button className="bg-green-600 hover:bg-green-900 text-white font-bold py-2 px-4 rounded mb-4">
-            Learn More
-          </button>
-        </div>
-      </div> */}
-
       <section className="bg-center bg-no-repeat bg-[url('assets/images/blocklandbg.webp')] bg-gray-700 bg-blend-multiply">
         <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
@@ -84,33 +39,6 @@ const Home = () => {
             >
               Brokerage Details
             </button>
-            {/* <a
-              href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900"
-            >
-              Get started
-              <svg
-                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-            <a
-              href="#"
-              className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
-            >
-              Learn more
-            </a> */}
           </div>
         </div>
       </section>
