a90b2d4aeb1bec1885a7e4c19310d8e21b542f71
Vinuri Disara <vinuridisara2020@gmail.com>
2024-05-08 14:53:20 +0530

    Update Advertisement model to enforce image limits and add validation messages

--- backend/Models/advertisement.js
+++ backend/Models/advertisement.js
@@ -3,47 +3,49 @@ const { Schema, model, models } = pkg;
 // import mongooseUniqueValidator from "mongoose-unique-validator";
 
 const AdvertisementSchema = new Schema({
-    title: {
-        type: String,
-        required: [true, "Please enter the title"],
-    },
+  title: {
+    type: String,
+    required: [true, "Please enter the title"],
+  },
 
-    description: {
-        type: String,
-        required: [true, "Please enter the description"],
-    },
+  description: {
+    type: String,
+    required: [true, "Please enter the description"],
+  },
 
-    ownerName: {
-        type: String,
-        required: [true, "Please enter the owner's name"],
-    },
+  ownerName: {
+    type: String,
+    required: [true, "Please enter the owner's name"],
+  },
 
-    contactNumber: {
-        type: String,
-        required: [true, "Please enter contact number"],
-    },
+  contactNumber: {
+    type: String,
+    required: [true, "Please enter contact number"],
+  },
 
-    images: {
-        type: [String], // Array of image URLs
-        required: [true, "Please enter the images"],
-    },
+  price: {
+    type: Number,
+    required: [true, "Please enter the price"],
+  },
 
-    price: {
-        type: Number,
-        required: [true, "Please enter the price"],
-    },
+  location: {
+    type: String,
+    required: [true, "Please enter the location"],
+  },
 
-    location: {
-        type: String,
-        required: [true, "Please enter the location"],
-    },
+  images: {
+    type: [String],
+    maxlength: [5, "Maximum 5 images are allowed"],
+    minlength: [1, "At least 1 image is required"],
+  },
 
-    status: {
-        type: Number,
-        default: 0,         // 0 - Pending, 1 - Approved, 2 - Rejected
-    }
-});  
+  status: {
+    type: Number,
+    default: 0, // 0 - Pending, 1 - Approved, 2 - Rejected
+  },
+});
 
-const Advertisement = models.Advertisement || model("Advertisement", AdvertisementSchema);
+const Advertisement =
+  models.Advertisement || model("Advertisement", AdvertisementSchema);
 
 export default Advertisement;
