9fcde5a63e5dbafdb7d305bd91dd8f9f5a6cec27
Vinuri Disara <vinuridisara2020@gmail.com>
2024-05-10 01:02:32 +0530

    feat: Add BookLawyer model for lawyer booking data

---
+++ backend/Models/booklawyer.js
@@ -0,0 +1,41 @@
+import pkg from "mongoose";
+const { Schema, model, models } = pkg;
+import bcrypt from "bcrypt";
+import mongooseUniqueValidator from "mongoose-unique-validator";
+
+const BookLawyerSchema = new Schema({
+  fullName: {
+    type: String,
+    required: [true, "Please enter Full Name"],
+  },
+
+  contactNumber: {
+    type: String,
+    required: [true, "Please enter Contact Number"],
+    unique: true,
+  },
+
+  nicNumber: {
+    type: String,
+    required: [true, "Please enter Address"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter Address"],
+  },
+
+  propertyType: {
+    type: String,
+    required: [true, "Please enter City"],
+  },
+});
+
+// Validate the email and contact number
+BookLawyerSchema.plugin(mongooseUniqueValidator, {
+  message: "Email or Contact Number already exists. Duplicate key",
+});
+
+const BookLawyer = models.BookLawyer || model("BookLawyer", BookLawyerSchema);
+
+export default BookLawyer;
