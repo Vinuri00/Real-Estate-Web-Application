d2e874ec6861056df015d407b262aeead0c31c5c
Vinuri Disara <vinuridisara2020@gmail.com>
2024-05-12 13:17:18 +0530

    refactor: Add LawyerModel and update BrokersModel in brokers.js
    This code change adds the LawyerModel to the brokers.js file, allowing for the management of lawyers in the application. It also updates the BrokersModel to include additional fields related to lawyers, such as companyName, licenseNumber, yearsOfExperience, image, and status. These changes improve the functionality and organization of the application.

--- Lawyer/src/models/brokers.js
+++ Lawyer/src/models/brokers.js
@@ -6,3 +6,30 @@ export const BrokerModel = {
     address: null,
     city: null,
 };
+
+export const LawyerModel = {
+    _id: null,
+    userId: {
+      _id: null,
+      fullName: null,
+      userName: null,
+      email: null,
+      contactNumber: null,
+    },
+    lawyerId: {
+      _id: null,
+      fullName: null,
+      userName: null,
+      email: null,
+      contactNumber: null,
+      companyName: null,
+      licenseNumber: null,
+      yearsOfExperience: null,
+      image: null,
+      status: 0,
+    },
+    userNICNumber: null,
+    propertyType: null,
+    date: "2024-10-08T18:30:00.000Z",
+};
+  
