bcc2362fa6b0fdab67d3a0499d0414db6f90ba72
Vinuri Disara <vinuridisara2020@gmail.com>
2024-05-12 13:17:23 +0530

    refactor: Remove unused lawyer, offer, and advertisement models

--- Lawyer/src/pages/dashboard/index.js
+++ Lawyer/src/pages/dashboard/index.js
@@ -3,9 +3,3 @@ export * from "@/pages/dashboard/Home";
 // Brokers
 export * from "@/pages/dashboard/AddBroker" 
 export * from "@/pages/dashboard/UpdateBroker" 
-
-// Lawyers
-export * from "@/pages/dashboard/Lawyer"
-
-// Advertisement
-export * from "@/pages/dashboard/Advertisement"
