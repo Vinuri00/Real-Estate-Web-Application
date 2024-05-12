dced493d5e8593f60f43977d2d6809feb3283557
Vinuri Disara <vinuridisara2020@gmail.com>
2024-05-12 13:17:11 +0530

    refactor: Update axios.js API endpoints for lawyers and bookings
    This code change updates the API endpoints in the axios.js file to reflect the changes made in the Lawyer application. The endpoints related to brokers are replaced with endpoints for lawyers and bookings. The unused endpoints for advertisements and offers are removed. These updates align the API endpoints with the current functionality of the application and improve code organization.

--- Lawyer/src/api/axios.js
+++ Lawyer/src/api/axios.js
@@ -12,18 +12,8 @@ export const GET_ONE_BROKER = "/brokers/get-one"; // broker: get one broker
 export const GET_ALL_BROKERS = "/brokers/get-all"; // broker: get all brokers
 export const UPDATE_BROKER_BY_ID_URL = "/brokers/update"; // broker: update broker
 export const DELETE_BROKER_BY_ID_URL = "/brokers/delete" // broker: delete broker
-
-// Lawyers
-export const GET_ALL_LAWYERS = "/lawyers/get-all"; // lawyer: get all lawyers
-export const DELETE_LAWYER_BY_ID_URL = "/lawyers/delete"; // lawyer: delete lawyer
-export const APPROVE_LAWYER = "/lawyers/approve"; // lawyer: delete lawyer
-export const REJECT_LAWYER = "/lawyers/reject"; // lawyer: delete lawyer
-
-// Advertisement
-export const GET_ALL_ADVERTISEMENTS = "/advertisements/get-all"; // advertisement: get all advertisements
-export const APPROVE_ADVERTISEMENT = "/advertisement/approve"; // advertisement: approve advertisement
-export const REJECT_ADVERTISEMENT = "/advertisements/reject"; // advertisement: reject advertisement
-/*export const DELETE_ADVERTISEMENT = "/advertisement/delete"; // advertisement: delete advertisement*/  
+export const LAWYER_LOGIN = "/lawyers/login" // broker: delete broker
+export const GET_BOOKINGS_BY_LAWYERID = "/booklawyer/get-one" // broker: delete broker
 
 // Payment
 export const GET_PAYMENTS = "/payment";
